1e58790108757d822e9e9087b15600b033d6d9e7
심민성 <75308777+owl1753@users.noreply.github.com>
2021-10-30 20:28:49 +0900

    Create Probability Calculation

--- Make_Friends/src/Manager.java
+++ Make_Friends/src/Manager.java
@@ -1,15 +1,42 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Manager {
 	private static ArrayList<User> users = new ArrayList<User>();
 	
 	public static void AddUser(String userID, String userName, String userGrade, String userClub, String userDepartment, String userClass, ArrayList<String> userInterests) {
-		users.add(new User(userID, userName, userGrade, userClub, userDepartment, userClass, userInterests));
+		users.add(new User(users.size(), userID, userName, userGrade, userClub, userDepartment, userClass, userInterests));
 	}
 	
 	public static void PrintAllUsers() {
 		for (int i = 0; i < users.size(); i++) {
 			users.get(i).PrintUserInfo();
 		}
+	}
+	
+	public static int FindFriendDistance(int start, int end) {
+		int[] dist = new int[users.size()];
+		Queue<Integer> q = new LinkedList<>();
+
+		q.add(start);
+		dist[0] = 0;
+		for (int i = 1; i < dist.length; i++) {
+			dist[i] = -1;
+		}
+		
+		while(!q.isEmpty()) {
+			int here = q.poll();
+			ArrayList<User> friends = users.get(here).GetUserFriends();
+			for (int i = 0; i < friends.size(); i++) {
+				int next = friends.get(i).GetUserNumber();
+				if (dist[next] == -1) {
+					dist[next] = dist[here] + 1;
+					q.add(next);
+				}
+			}
+		}
+		
+		return dist[end];
 	}
 }
--- Make_Friends/src/User.java
+++ Make_Friends/src/User.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class User {	
 	private ArrayList<User> friends = new ArrayList<User>();
+	private int userNumber;
 	private String userID;
 	private String userName;
 	private String userGrade;
@@ -10,7 +11,8 @@ public class User {
 	private String userClass;
 	private ArrayList<String> userInterests;
 	
-	User(String userID, String userName, String userGrade, String userClub, String userDepartment, String userClass, ArrayList<String> userInterests) {
+	User(int userNumber, String userID, String userName, String userGrade, String userClub, String userDepartment, String userClass, ArrayList<String> userInterests) {
+		this.userNumber = userNumber;
 		this.userID = userID;
 		this.userName = userName;
 		this.userGrade = userGrade;
@@ -20,8 +22,32 @@ public class User {
 		this.userInterests = userInterests;
 	}
 	
+	int GetUserNumber() {
+		return this.userNumber;
+	}
+	
+	ArrayList<User> GetUserFriends() {
+		return this.friends;
+	}
+	
+	String GetUserInterest(int index) {
+		return this.userInterests.get(index);
+	}
+	
+	String GetUserClass() {
+		return this.userClass;
+	}
+	
+	String GetUserDepartment() {
+		return this.userDepartment;
+	}
+	
 	String GetUserID() {
 		return this.userID;
+	}
+	
+	String GetUserClub() {
+		return this.userClub;
 	}
 	
 	void AddFriend(User u) {
@@ -48,5 +74,37 @@ public class User {
 			System.out.print(" " + "관심사" + (i + 1) + " : " + userInterests.get(i));
 		}
 		System.out.println();
+	}
+	
+	public double CalculateProbability(User friend) {
+		double prob = 0.0;
+		
+		// 동아리, 가중치 35%, 같으면 1, 아니면 0
+		if (this.userClub.equals(friend.GetUserClub())) {
+			prob += 0.35;
+		}
+		
+		// 친구 거리, 가중치 30%, 2^-(x-1)
+		prob += Math.pow(2.0, -(Manager.FindFriendDistance(userNumber, friend.userNumber) - 1));
+		
+		// 반, 가중치 20%, 같은학과 다른반 0.6, 나머지 0.3
+		if (this.userClass.equals(friend.GetUserClass())) {
+			prob += 0.2;
+		}
+		else if (this.userDepartment.equals(friend.GetUserDepartment())) {
+			prob += (0.2 * 0.6);
+		}
+		else {
+			prob += (0.2 * 0.3);
+		}
+		
+		// 관심사, 가중치 15%, 같은 관심사 1당 0.2
+		for (int i = 0; i < 5; i++) {
+			if (this.userInterests.contains(friend.GetUserInterest(i))) {
+				prob += (0.15 * 0.2);
+			}
+		}
+			
+		return prob;
 	}
 }
